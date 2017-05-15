df450e83116121b8188f526dc91f7194f822e84a
Rex Tsou <akccakccwww@gmail.com>
2017-05-15 16:15:57 +0800

    update darklight core JS and demo

--- demo/darklight.js
+++ demo/darklight.js
@@ -1,81 +1,116 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function () {
-  // Element.matches() polyfill from MDN: IE9 up
-  if (!Element.prototype.matches) {
-    Element.prototype.matches = Element.prototype.matchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.msMatchesSelector || Element.prototype.oMatchesSelector || Element.prototype.webkitMatchesSelector || function (s) {
-      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-          i = matches.length;
-      while (--i >= 0 && matches.item(i) !== this) {}
-      return i > -1;
-    };
-  }
-  var box = document.querySelector('.darklight');
-  var btnOpens = document.querySelectorAll('[data-darklight]');
-  var btnClose = document.querySelector('.darklight .darklight-close');
-  var openBox = function openBox(targetContent) {
-    history.pushState(null, null, document.URL); // prevent go back out of page
-    box.classList.add('is-active');
-    box.querySelector('[data-darklight-content=' + targetContent + ']').classList.add('is-active');
-  };
-  var closeBox = function closeBox() {
-    var popHistory = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : true;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-    if (box.classList.contains('is-active')) {
-      box.classList.remove('is-active');
-      box.querySelectorAll('.darklight-content').forEach(function (content) {
-        return content.classList.remove('is-active');
-      });
-      if (popHistory === true) {
-        history.go(-1);
-      }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(function (window) {
+  var Darklight = function () {
+    function Darklight() {
+      _classCallCheck(this, Darklight);
+
+      this.version = '0.1.0';
+      this.author = 'Rex Tsou <akccakccwww@gmail.com>';
+      this.bugs = 'https://github.com/akccakcctw/darklight/issues';
+      this.license = 'WTFPL';
     }
-  };
-  if (box !== null) {
-    Array.from(btnOpens).forEach(function (btnOpen) {
-      return btnOpen.addEventListener('click', function () {
-        openBox(btnOpen.dataset.darklight);
-      });
-    });
-    btnClose.addEventListener('click', function () {
-      closeBox();
-    });
 
-    document.addEventListener('mouseup', function (e) {
-      var content = document.querySelector('.darklight .darklight-content');
-      // click outer space to close darklight
-      if (!e.target.matches('.darklight .darklight-content') && !content.contains(e.target)) {
-        closeBox();
+    _createClass(Darklight, [{
+      key: 'openBox',
+      value: function openBox(targetContent) {
+        history.pushState(null, null, document.URL); // prevent go back out of page
+        this.box.classList.add('is-active');
+        this.box.querySelector('[data-darklight-content=' + targetContent + ']').classList.add('is-active');
       }
-    });
+    }, {
+      key: 'closeBox',
+      value: function closeBox() {
+        var popHistory = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : true;
 
-    document.addEventListener('keyup', function (e) {
-      if (e.keyCode === 27 || e.keyCode === 8) {
-        // 27(esc), 8(backspace)
-        closeBox();
+        if (this.box.classList.contains('is-active')) {
+          this.box.classList.remove('is-active');
+          this.box.querySelectorAll('.darklight-content').forEach(function (content) {
+            return content.classList.remove('is-active');
+          });
+          if (popHistory === true) {
+            history.go(-1);
+          }
+        }
       }
-    });
-    window.addEventListener('popstate', function () {
-      closeBox(false);
-    });
+    }, {
+      key: 'init',
+      value: function init(_ref) {
+        var _this = this;
 
-    // close icon
-    var closeIcon = document.querySelector('.darklight .darklight-icon use');
-    var closeIconLink = closeIcon.getAttribute('xlink:href').replace('#close', '');
+        var _ref$box = _ref.box,
+            box = _ref$box === undefined ? document.querySelector('.darklight') : _ref$box,
+            _ref$btnOpens = _ref.btnOpens,
+            btnOpens = _ref$btnOpens === undefined ? document.querySelectorAll('[data-darklight]') : _ref$btnOpens,
+            _ref$btnClose = _ref.btnClose,
+            btnClose = _ref$btnClose === undefined ? document.querySelector('.darklight .darklight-close') : _ref$btnClose;
 
-    var cW = document.body.clientWidth;
-    if (cW <= 767) {
-      var newCloseIconLink = closeIconLink + '#close-inverse';
-      closeIcon.setAttribute('xlink:href', newCloseIconLink);
-    }
-    window.addEventListener('resize', function () {
-      if (cW <= 767) {
-        var _newCloseIconLink = closeIconLink + '#close-inverse';
-        closeIcon.setAttribute('xlink:href', _newCloseIconLink);
-      } else if (cW > 767) {
-        var _newCloseIconLink2 = closeIconLink + '#close';
-        closeIcon.setAttribute('xlink:href', _newCloseIconLink2);
+        // configs
+        this.box = box;
+        this.btnOpens = btnOpens;
+        this.btnClose = btnClose;
+
+        // default functions
+        if (this.box !== null) {
+          Array.from(this.btnOpens).forEach(function (btnOpen) {
+            return btnOpen.addEventListener('click', function () {
+              _this.openBox(btnOpen.dataset.darklight);
+            });
+          });
+          this.btnClose.addEventListener('click', function () {
+            _this.closeBox();
+          });
+
+          document.addEventListener('mouseup', function (e) {
+            var content = document.querySelector('.darklight .darklight-content');
+            // click outer space to close darklight
+            if (!e.target.matches('.darklight .darklight-content') && !content.contains(e.target)) {
+              _this.closeBox();
+            }
+          });
+
+          document.addEventListener('keyup', function (e) {
+            if (e.keyCode === 27 || e.keyCode === 8) {
+              // 27(esc), 8(backspace)
+              _this.closeBox();
+            }
+          });
+          window.addEventListener('popstate', function () {
+            _this.closeBox(false);
+          });
+
+          // close icon
+          var closeIcon = document.querySelector('.darklight .darklight-icon use');
+          var closeIconLink = closeIcon.getAttribute('xlink:href').replace('#close', '');
+
+          var cW = document.body.clientWidth;
+          if (cW <= 767) {
+            var newCloseIconLink = closeIconLink + '#close-inverse';
+            closeIcon.setAttribute('xlink:href', newCloseIconLink);
+          }
+          window.addEventListener('resize', function () {
+            if (cW <= 767) {
+              var _newCloseIconLink = closeIconLink + '#close-inverse';
+              closeIcon.setAttribute('xlink:href', _newCloseIconLink);
+            } else if (cW > 767) {
+              var _newCloseIconLink2 = closeIconLink + '#close';
+              closeIcon.setAttribute('xlink:href', _newCloseIconLink2);
+            }
+          });
+        }
       }
-    });
+    }]);
+
+    return Darklight;
+  }();
+
+  if (typeof window.Darklight === 'undefined') {
+    window.Darklight = new Darklight();
+  } else {
+    console.error('Darklight already defined.');
   }
-});
+})(window);
--- src/js/darklight.js
+++ src/js/darklight.js
@@ -1,74 +1,83 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Element.matches() polyfill from MDN: IE9 up
-  if (!Element.prototype.matches) {
-    Element.prototype.matches =
-      Element.prototype.matchesSelector ||
-      Element.prototype.mozMatchesSelector ||
-      Element.prototype.msMatchesSelector ||
-      Element.prototype.oMatchesSelector ||
-      Element.prototype.webkitMatchesSelector ||
-      function (s) {
-        var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-          i = matches.length;
-        while (--i >= 0 && matches.item(i) !== this) { }
-        return i > -1;
-      };
-  }
-  const box = document.querySelector('.darklight');
-  const btnOpens = document.querySelectorAll('[data-darklight]');
-  const btnClose = document.querySelector('.darklight .darklight-close');
-  const openBox = (targetContent) => {
-    history.pushState(null, null, document.URL); // prevent go back out of page
-    box.classList.add('is-active');
-    box.querySelector(`[data-darklight-content=${targetContent}]`).classList.add('is-active');
-  };
-  const closeBox = (popHistory = true) => {
-    if (box.classList.contains('is-active')) {
-      box.classList.remove('is-active');
-      box.querySelectorAll('.darklight-content').forEach(content => content.classList.remove('is-active'));
-      if (popHistory === true) {
-        history.go(-1);
+(function (window) {
+  class Darklight {
+    constructor() {
+      this.version = '0.1.0';
+      this.author = 'Rex Tsou <akccakccwww@gmail.com>';
+      this.bugs = 'https://github.com/akccakcctw/darklight/issues';
+      this.license = 'WTFPL';
+    }
+    openBox(targetContent) {
+      history.pushState(null, null, document.URL); // prevent go back out of page
+      this.box.classList.add('is-active');
+      this.box.querySelector(`[data-darklight-content=${targetContent}]`).classList.add('is-active');
+    }
+    closeBox(popHistory = true) {
+      if (this.box.classList.contains('is-active')) {
+        this.box.classList.remove('is-active');
+        this.box.querySelectorAll('.darklight-content').forEach(content => content.classList.remove('is-active'));
+        if (popHistory === true) {
+          history.go(-1);
+        }
       }
     }
-  };
-  if (box !== null) {
-    Array.from(btnOpens).forEach(btnOpen => btnOpen.addEventListener('click', () => {
-      openBox(btnOpen.dataset.darklight);
-    }));
-    btnClose.addEventListener('click', () => { closeBox(); });
+    init({
+      box = document.querySelector('.darklight'),
+      btnOpens = document.querySelectorAll('[data-darklight]'),
+      btnClose = document.querySelector('.darklight .darklight-close'),
+    }) {
+      // configs
+      this.box = box;
+      this.btnOpens = btnOpens;
+      this.btnClose = btnClose;
 
-    document.addEventListener('mouseup', (e) => {
-      const content = document.querySelector('.darklight .darklight-content');
-      // click outer space to close darklight
-      if (!e.target.matches('.darklight .darklight-content') && !content.contains(e.target)) {
-        closeBox();
+      // default functions
+      if (this.box !== null) {
+        Array.from(this.btnOpens).forEach(btnOpen => btnOpen.addEventListener('click', () => {
+          this.openBox(btnOpen.dataset.darklight);
+        }));
+        this.btnClose.addEventListener('click', () => { this.closeBox(); });
+
+        document.addEventListener('mouseup', (e) => {
+          const content = document.querySelector('.darklight .darklight-content');
+          // click outer space to close darklight
+          if (!e.target.matches('.darklight .darklight-content') && !content.contains(e.target)) {
+            this.closeBox();
+          }
+        });
+
+        document.addEventListener('keyup', (e) => {
+          if (e.keyCode === 27 || e.keyCode === 8) { // 27(esc), 8(backspace)
+            this.closeBox();
+          }
+        });
+        window.addEventListener('popstate', () => { this.closeBox(false); });
+
+        // close icon
+        const closeIcon = document.querySelector('.darklight .darklight-icon use');
+        const closeIconLink = closeIcon.getAttribute('xlink:href').replace('#close', '');
+
+        const cW = document.body.clientWidth;
+        if (cW <= 767) {
+          const newCloseIconLink = `${closeIconLink}#close-inverse`;
+          closeIcon.setAttribute('xlink:href', newCloseIconLink);
+        }
+        window.addEventListener('resize', () => {
+          if (cW <= 767) {
+            const newCloseIconLink = `${closeIconLink}#close-inverse`;
+            closeIcon.setAttribute('xlink:href', newCloseIconLink);
+          } else if (cW > 767) {
+            const newCloseIconLink = `${closeIconLink}#close`;
+            closeIcon.setAttribute('xlink:href', newCloseIconLink);
+          }
+        });
       }
-    });
-
-    document.addEventListener('keyup', (e) => {
-      if (e.keyCode === 27 || e.keyCode === 8) { // 27(esc), 8(backspace)
-        closeBox();
-      }
-    });
-    window.addEventListener('popstate', () => { closeBox(false); });
-
-    // close icon
-    const closeIcon = document.querySelector('.darklight .darklight-icon use');
-    const closeIconLink = closeIcon.getAttribute('xlink:href').replace('#close', '');
-
-    const cW = document.body.clientWidth;
-    if (cW <= 767) {
-      const newCloseIconLink = `${closeIconLink}#close-inverse`;
-      closeIcon.setAttribute('xlink:href', newCloseIconLink);
     }
-    window.addEventListener('resize', () => {
-      if (cW <= 767) {
-        const newCloseIconLink = `${closeIconLink}#close-inverse`;
-        closeIcon.setAttribute('xlink:href', newCloseIconLink);
-      } else if (cW > 767) {
-        const newCloseIconLink = `${closeIconLink}#close`;
-        closeIcon.setAttribute('xlink:href', newCloseIconLink);
-      }
-    });
+
   }
-});
+
+  if (typeof (window.Darklight) === 'undefined') {
+    window.Darklight = new Darklight();
+  } else {
+    console.error('Darklight already defined.');
+  }
+}(window));
